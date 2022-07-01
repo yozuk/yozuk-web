e5ce418b979b40d7de912622e717d7fa9bfb33b3
picoHz <53327867+picoHz@users.noreply.github.com>
2022-07-01 23:35:43 +0900

    button

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,8 +5,23 @@ const count = ref(0)
 </script>
 
 <template>
-  <div>
-    <img id="yozuk-logo" alt="Yozuk logo" src="./../assets/logo.svg" />
+  <div class="absolute inset-0 flex flex-col">
+    <div class="grow"></div>
+    <div class="grow-0">
+      <h1 class="text-6xl text-gray-900 font-bold">Yozuk</h1>
+      <h2 class="text-4xl text-gray-500">Chatbot for <span class="underline">programmers</span></h2>
+    </div>
+    <div class="grow-0 my-8">
+      <img id="yozuk-logo" class="mx-auto" alt="Yozuk logo" src="./../assets/logo.svg" />
+    </div>
+    <div class="grow-0">
+      <button class="text-2xl mx-2 px-3 py-1 border-2 bg-gray-200 border-gray-600 rounded"><a target="_blank"
+          href="https://docs.yozuk.com/">Documentation</a></button>
+      <button class="text-2xl mx-2 px-3 py-1 border-2 border-gray-600 text-gray-100 bg-gray-500 rounded"><a
+          target="_blank" href="https://github.com/yozuk/yozuk">GitHub</a></button>
+    </div>
+    <div class=" grow">
+    </div>
   </div>
 </template>
 
@@ -14,5 +29,11 @@ const count = ref(0)
 #yozuk-logo {
   max-width: 15rem;
   max-height: 15rem;
+}
+
+h1,
+h2,
+button {
+  font-family: 'Barlow Condensed', sans-serif;
 }
 </style>
