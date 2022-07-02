3e7aff61adac8e0a35022dea9ec3b9e63c3b771c
picoHz <53327867+picoHz@users.noreply.github.com>
2022-07-02 10:46:53 +0900

    compact layout

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -5,7 +5,7 @@ const count = ref(0)
 </script>
 
 <template>
-  <div class="hidden md:flex fixed inset-0 right-1/2 flex-col border-r border-gray-400">
+  <div class="hidden md:flex fixed inset-0 right-1/2 flex-col border-r bg-gray-300 border-gray-300">
     <div class="grow"></div>
     <div class="grow-0">
       <h1 class="text-6xl text-gray-900 font-bold">Yozuk</h1>
@@ -14,67 +14,33 @@ const count = ref(0)
     <div class="grow-0 my-8">
       <img id="yozuk-logo" class="mx-auto" alt="Yozuk logo" src="./../assets/logo.svg" />
     </div>
-    <div class="grow-0">
-      <button class="text-2xl mx-2 px-3 py-1 border-2 bg-gray-200 border-gray-600 rounded"><a target="_blank"
-          href="https://docs.yozuk.com/">Documentation</a></button>
-      <button class="text-2xl mx-2 px-3 py-1 border-2 border-gray-600 text-gray-100 bg-gray-500 rounded"><a
-          target="_blank" href="https://github.com/yozuk/yozuk">GitHub</a></button>
+    <div class="grow-0"><a target="_blank" href="https://docs.yozuk.com/">
+        <button class="text-2xl mx-2 px-3 py-1 border-2 bg-gray-200 border-gray-600 rounded">Documentation</button></a>
+      <a target="_blank" href="https://github.com/yozuk/yozuk"><button
+          class="text-2xl mx-2 px-3 py-1 border-2 border-gray-600 text-gray-100 bg-gray-500 rounded">GitHub</button></a>
     </div>
     <div class=" grow">
     </div>
   </div>
-  <div class="static md:absolute top-0 right-0 left-1/2">
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
-    <h1>aaaaaa</h1>
+  <div class="chatbox static md:absolute top-0 right-0 left-1/2">
+    <div class="md:hidden flex flex-col py-6 bg-gray-300">
+      <div class="grow"></div>
+      <div class="grow-0">
+        <h1 class="text-5xl text-gray-900 font-bold">Yozuk</h1>
+        <h2 class="text-2xl text-gray-500">Chatbot for <span class="underline">programmers</span></h2>
+      </div>
+      <div class="grow-0 my-3">
+        <img id="yozuk-logo" class="mx-auto" alt="Yozuk logo" src="./../assets/logo.svg" />
+      </div>
+      <div class="grow-0"><a target="_blank" href="https://docs.yozuk.com/">
+          <button
+            class="text-2xl mx-2 px-2 py-1 border-2 bg-gray-200 border-gray-600 rounded">Documentation</button></a>
+        <a target="_blank" href="https://github.com/yozuk/yozuk"><button
+            class="text-2xl mx-2 px-2 py-1 border-2 border-gray-600 text-gray-100 bg-gray-500 rounded">GitHub</button></a>
+      </div>
+      <div class=" grow">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -82,6 +48,11 @@ const count = ref(0)
 #yozuk-logo {
   max-width: 15rem;
   max-height: 15rem;
+}
+
+.chatbox #yozuk-logo {
+  max-width: 10rem;
+  max-height: 10rem;
 }
 
 h1,
