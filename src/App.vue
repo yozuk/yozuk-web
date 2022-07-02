c483264424b8b3653ea703a02b4b3a5ad352ee99
picoHz <53327867+picoHz@users.noreply.github.com>
2022-07-02 09:15:32 +0900

    layout

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Main from './components/Main.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <Main />
 </template>
 
 <style>
---
+++ src/components/Main.vue
@@ -0,0 +1,92 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="hidden md:flex fixed inset-0 right-1/2 flex-col border-r border-gray-400">
+    <div class="grow"></div>
+    <div class="grow-0">
+      <h1 class="text-6xl text-gray-900 font-bold">Yozuk</h1>
+      <h2 class="text-4xl text-gray-500">Chatbot for <span class="underline">programmers</span></h2>
+    </div>
+    <div class="grow-0 my-8">
+      <img id="yozuk-logo" class="mx-auto" alt="Yozuk logo" src="./../assets/logo.svg" />
+    </div>
+    <div class="grow-0">
+      <button class="text-2xl mx-2 px-3 py-1 border-2 bg-gray-200 border-gray-600 rounded"><a target="_blank"
+          href="https://docs.yozuk.com/">Documentation</a></button>
+      <button class="text-2xl mx-2 px-3 py-1 border-2 border-gray-600 text-gray-100 bg-gray-500 rounded"><a
+          target="_blank" href="https://github.com/yozuk/yozuk">GitHub</a></button>
+    </div>
+    <div class=" grow">
+    </div>
+  </div>
+  <div class="static md:absolute top-0 right-0 left-1/2">
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+    <h1>aaaaaa</h1>
+  </div>
+</template>
+
+<style scoped>
+#yozuk-logo {
+  max-width: 15rem;
+  max-height: 15rem;
+}
+
+h1,
+h2,
+button {
+  font-family: 'Barlow Condensed', sans-serif;
+}
+</style>
