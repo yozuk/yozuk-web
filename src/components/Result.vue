c23ad3f455df970abdaa80f03d7b4337de5350a7
picoHz <picoHz@outlook.com>
2022-08-01 09:16:58 +0900

    color

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -11,6 +11,12 @@ const props = defineProps({
     <div v-for="output in msg.outputs" :key="output.title"
         class="rounded overflow-hidden mx-3 bg-gray-600 text-gray-200 text-left border-b border-gray-500 shadow-xl">
         <h3 class="text-sm font-bold py-1 md:py-2 px-2 md:px-4">Yozuk: {{ output.title }}</h3>
+        <div v-for="metadata in output.metadata" :key="metadata">
+            <div v-if="metadata.type === 'color'"
+                class="max-w-lg bg-repeat h-4 md:h-6" :style="{
+          'background-image': `linear-gradient(0deg, ${metadata.color}, ${metadata.color}), url(\'check.png\')`,
+            }"></div>
+        </div>
         <Block v-for=" block in output.blocks" :key="block" :block="block" />
     </div>
 </template>
