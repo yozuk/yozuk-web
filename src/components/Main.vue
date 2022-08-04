f828c1df72c42948a634b50fa537f8347508e602
picoHz <53327867+picoHz@users.noreply.github.com>
2022-08-04 20:42:39 +0900

    remove send button

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -234,22 +234,6 @@ function onWheel(event) {
                   </path>
                 </svg>
               </button>
-              <button :disabled="chatHistory.length === 0" @click="run($refs.command.value)" class="
-                text-gray-700
-                rounded-full
-                text-xs
-                p-2.5
-                text-center
-                inline-flex
-                items-center
-              ">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                  stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                  class="feather feather-send">
-                  <line x1="22" y1="2" x2="11" y2="13"></line>
-                  <polygon points="22 2 15 22 11 13 2 9 22 2"></polygon>
-                </svg>
-              </button>
               <input type="file" ref="file" @change="addFile" style="display: none" />
             </div>
           </div>
